b7ca21f710bc4a02466031fd741296c892701376
FSPonte <fsponte@outlook.com>
2025-07-28 03:43:36 +0100

    Common dependencies, datatypes, structs and constant expressions

---
+++ fsponte/problem_3/header.hpp
@@ -0,0 +1,42 @@
+#ifndef _HEADER_HPP_
+#define _HEADER_HPP_
+
+// Dependencies
+#include <cmath>
+#include <string>
+#include <vector>
+
+using type_t = long double; // Data type
+
+typedef std::vector<std::string> file_lines_t; // Lines of a file
+typedef std::vector<std::vector<type_t>> table_t; // Matrix of numerical values
+typedef std::vector<type_t> set_t; // Set of values
+
+// Parameters for the Lennard-Jones potential
+struct params_t
+{
+	type_t
+		dept, // Dept of the potencial (epsilon)
+		zero; // Zero of the potencial (sigma)
+
+	type_t norm(void)
+	{
+		return this->dept * this->dept + this->zero * this->zero;
+	}
+
+	void operator -= (const params_t p)
+	{
+		this->dept -= p.dept;
+		this->zero -= p.zero;
+	}
+
+	void operator /= (const type_t scalar)
+	{
+		this->dept /= scalar;
+		this->zero /= scalar;
+	}
+};
+
+constexpr const char csv_file_path[] = "../../problems/lennard_jones.csv"; // File path
+
+#endif // _HEADER_HPP_
