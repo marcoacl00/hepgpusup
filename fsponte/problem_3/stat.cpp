10999db2af041d7438796814d6e6e59ed997632a
FSPonte <fsponte@outlook.com>
2025-07-28 12:21:56 +0100

    Removed R^2 gradient and added a cost function with respective gradient

--- fsponte/problem_3/stat.cpp
+++ fsponte/problem_3/stat.cpp
@@ -11,6 +11,43 @@ type_t drand48(const type_t min, const type_t max) noexcept(false)
 	return (max - min) * drand48() + min;
 }
 
+type_t cost_fn(const set_t dataset, const set_t fit) noexcept(false)
+{
+	const unsigned long DIM = dataset.size();
+
+	if (DIM != fit.size())
+		throw "Sets with different dimensions";
+
+	type_t sum = 0;
+
+	for (unsigned long i = 0; i < DIM; ++i)
+		sum += (fit[i] - dataset[i]) * (fit[i] - dataset[i]);
+
+	return sum;
+}
+
+params_t cost_fn_grad(const set_t dataset, const set_t fit, const set_t fit_der_dept, const set_t fit_der_zero) noexcept(false)
+{
+	const unsigned long DIM = dataset.size();
+
+	if (DIM != fit.size() || DIM != fit_der_dept.size() || DIM != fit_der_zero.size())
+		throw "Sets with different dimensions";
+	
+	type_t
+		var,
+		sum_dept = 0,
+		sum_zero = 0;
+
+	for (unsigned long i = 0; i < DIM; ++i)
+	{
+		var = fit[i] - dataset[i];
+		sum_dept += var * fit_der_dept[i];
+		sum_zero += var * fit_der_zero[i];
+	}
+
+	return params_t{2 * sum_dept, 2 * sum_zero};
+}
+
 type_t coeff_det(const set_t dataset, const set_t fit) noexcept(false)
 {
 	const unsigned long DIM = dataset.size();
@@ -39,43 +76,4 @@ type_t coeff_det(const set_t dataset, const set_t fit) noexcept(false)
     }
 
     return (ss_tot == 0) ? 0 : 1 - (ss_res / ss_tot);
-}
-
-params_t coeff_det_grad(const set_t dataset, const set_t fit, const set_t fit_der_dept, const set_t fit_der_zero) noexcept(false)
-{
-	const unsigned long DIM = dataset.size();
-
-	if (DIM != fit.size() || DIM != fit_der_dept.size() || DIM != fit_der_zero.size())
-		throw "Sets with different dimensions";
-	
-	type_t mean = 0;
-
-	// Calculate mean
-	{
-		for (auto value : dataset)
-			mean += value;
-
-		mean /= DIM;
-	}
-
-	type_t
-		ss_res = 0,
-    	ss_tot = 0,
-		ss_der_dept = 0,
-		ss_der_zero = 0;
-
-	for (unsigned long i = 0; i < DIM; ++i)
-	{
-		ss_res += dataset[i] - fit[i];
-		ss_tot += (dataset[i] - mean) * (dataset[i] - mean);
-		ss_der_dept += fit_der_dept[i];
-		ss_der_zero += fit_der_zero[i];
-	}
-
-	if (ss_tot == 0)
-		return params_t{0, 0};
-	
-	type_t coeff = 2 * ss_res / ss_tot;
-
-	return params_t{coeff * ss_der_dept, coeff * ss_der_zero};
 }
--- fsponte/problem_3/stat.hpp
+++ fsponte/problem_3/stat.hpp
@@ -14,6 +14,26 @@
 type_t drand48(type_t, type_t) noexcept(false);
 
 /**
+ * @brief Cost function
+ * @param d Dataset
+ * @param f Fit
+ * @return Cost value
+ * @throw Sets with different dimensions
+*/
+type_t cost_fn(set_t, set_t) noexcept(false);
+
+/**
+ * @brief Gradient of the cost function
+ * @param d Dataset
+ * @param f Fit
+ * @param fdd Fit derivative with respect to dept
+ * @param fdz Fit derivative with respect to zero
+ * @return Gradient of the cost function
+ * @throw Sets with different dimensions
+*/
+params_t cost_fn_grad(set_t, set_t, set_t, set_t) noexcept(false);
+
+/**
  * @brief Coefficient of determination
  * @param d Dataset
  * @param f Fit
@@ -21,16 +41,5 @@ type_t drand48(type_t, type_t) noexcept(false);
  * @throw Sets with different dimensions
 */
 type_t coeff_det(set_t, set_t) noexcept(false);
-
-/**
- * @brief Gradient of the coefficient of determination
- * @param d Dataset
- * @param f Fit
- * @param fdd Fit derivative with respect to dept
- * @param fdz Fit derivative with respect to zero
- * @return Gradient of R squared
- * @throw Sets with different dimensions
-*/
-params_t coeff_det_grad(set_t, set_t, set_t, set_t) noexcept(false);
 
 #endif // _STAT_HPP_
