62198a86c0d0ee7bd24a391f01178f293278a436
FSPonte <fsponte@outlook.com>
2025-08-13 23:31:10 +0100

    Changed the name of the file

--- fsponte/Numerical methods/problem_4/constexpr.hpp
+++ fsponte/Numerical methods/problem_4/constexpr.hpp
@@ -1,9 +1,9 @@
-#ifndef _PARAMS_HPP_
-#define _PARAMS_HPP_
+#ifndef _CONSTEXPR_HPP_
+#define _CONSTEXPR_HPP_
 
 constexpr unsigned long DIM = 3; // Dimension
 constexpr double
 	PLANCK_CONST = 6.62607015E-34, // Planck's constant (SI units)
 	CRIT_TEMP = 1; //  Critical temperature
 
-#endif // _PARAMS_HPP_
+#endif // _CONSTEXPR_HPP_
