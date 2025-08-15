96eddfd87a2097e4642d3ed13da5f2abb32af438
FSPonte <fsponte@outlook.com>
2025-08-15 12:55:49 +0100

    Added a header file with the simulation parameters

---
+++ fsponte/Numerical methods/problem_4/params.hpp
@@ -0,0 +1,11 @@
+#ifndef _PARAMS_HPP_
+#define _PARAMS_HPP_
+
+constexpr unsigned long
+	LAT_SPA = 1, // Lattice spacing
+	NUM_DIMS = 2; // Number of dimensions
+constexpr double
+	TEMP = 2.5, // Temperature
+	EXT_FLD_SRNT = 0.1; // External field strength
+
+#endif // _PARAMS_HPP_
